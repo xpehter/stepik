5b864cfc72802ad3c40f62f7a6658a73a3124452
Dmitry Kondrashov <xpeh18@ya.ru>
2017-10-12 04:56:39 +0500

    End solving 2.3_10

--- 2.3/s10.java
+++ 2.3/s10.java
@@ -1,5 +1,8 @@
 public class s10 {
     public static boolean isPalindrome(String text) {
-        return true;
+        String textOn = text.toLowerCase().replaceAll("[^0-9a-zA-Z]", "");
+        StringBuilder reversSB = new StringBuilder(text);
+        String revers = reversSB.reverse().toString().toLowerCase().replaceAll("[^0-9a-zA-Z]", "");
+        return revers.equals(textOn);
     }
 }
--- 2.3/stepic.java
+++ 2.3/stepic.java
@@ -1,18 +1,6 @@
 
 class stepic {
     public static void main(String[] args) {
-        System.out.println(s10.isPalindrome("p"));
-        //System.out.println(s9.isPowerOfTwo(0));
-        //System.out.println(Integer.toBinaryString(9));
-        //System.out.println(Integer.toBinaryString(-9));
-        //System.out.println(x == x * 2);
-        //int mod_x = x * (x>>>31) * (-1) + x * ((x>>>31) - 1) * (-1);
-        //int mod_x = (x>>>31) - 1;
-        //System.out.println("A" + ('\t' + '\u0003'));
-        //System.out.println(Integer.toBinaryString(9>>>31));
-/*
-*/
-/*
-*/
+        System.out.println(s10.isPalindrome("p1"));
     }
 }
