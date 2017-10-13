25c21b6e8fa93cd94bd1176f608ef2a58f12431d
Dmitry Kondrashov <xpeh18@ya.ru>
2017-10-13 05:57:13 +0500

    Started solving 2.4_9

---
+++ 2.4/s9.java
@@ -0,0 +1,8 @@
+
+public class s9 {
+    public static int[] mergeArrays(int[] a1, int[] a2) {
+        System.out.println(Arrays.toString(a1));
+        System.out.println(Arrays.toString(a2));
+        return 1;
+    }
+}
--- 2.4/stepic.java
+++ 2.4/stepic.java
@@ -1,6 +1,7 @@
 
 class stepic {
     public static void main(String[] args) {
-        System.out.println(s8.factorial(1));
+        //System.out.println(s8.factorial(1));
+        System.out.println(s9.mergeArrays({0, 2, 2}, {1, 3}));
     }
 }
