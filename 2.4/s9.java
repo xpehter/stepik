d553c10230758a461a33bf189c986c9017fa0b21
XPEH <xpeh18@ya.ru>
2018-02-17 01:03:03 +0500

    Slow working version

--- 2.4/s9.java
+++ 2.4/s9.java
@@ -1,107 +1,47 @@
 import java.util.Arrays;
-//import java.lang.*;
 
 public class s9 {
     public static int[] mergeArrays(int[] a1, int[] a2) {
 
-        //System.out.println(Arrays.toString(a1));
-        //System.out.println(Arrays.toString(a2));
+        //Без сортировки сливаем оба массива в один
+        int dlinna_a1 = a1.length;
+        int dlinna_a2 = a2.length;
+        int[] itogoviy_mas = new int[dlinna_a1 + dlinna_a2];
+        System.arraycopy(a1, 0, itogoviy_mas, 0, dlinna_a1);
+        System.arraycopy(a2, 0, itogoviy_mas, dlinna_a1, dlinna_a2);
+        int dlinna_itog_mas = itogoviy_mas.length;
 
-        int a1l = a1.length;
-        int a2l = a2.length;
-        int[] summ = new int[a1l + a2l];
-        System.arraycopy(a1, 0, summ, 0, a1l);
-        System.arraycopy(a2, 0, summ, a1l, a2l);
+        //Вычисляем первоначальный разрыв между сравниваемыми элементами с учётом фактора уменьшения
+        double d = dlinna_itog_mas / 1.247;
+        int shag_srav = (int) (d + 0.5);
 
-        //Самодельное заполнение массива summ из a1
-        //for (int i = 0; i <= (a1.length - 1); i++){
-        //    summ[i] = a1[i];
-        //}
-        //Самодельное заполнение массива summ из a2
-        //for (int i = a1.length; i <= (summ.length - 1); i++){
-        //    summ[i] = a2[i - a1.length];
-        //}
-
-        int summl = summ.length;
-
-        System.out.println(summl);
-        //System.out.println(Arrays.toString(summ));
-
-        // Попробовать реализовать последнюю из https://habrahabr.ru/post/204600/
-        int shag = 0;
-        int izm = 0;
-
-        // Сортировка расчёской
-        // и ... не работает, пишет
-        // Failed test #1. Wrong answer
-        // БЛИН!
-        double d = summl / 1.247;
-        int di = (int) (d + 0.5);
-        /*System.out.println(di);
-        d = di / 1.247;
-        di = (int) (d + 0.5);
-        System.out.println(di);*/
-        int check = 0;
-        int i = 0;
-        //for (int i = 0; i < summl; i++){
-        while (i == 0){
-            //System.out.println("Step: " +  shag++ + " " + Arrays.toString(summ));
-            if (di != check & di > 1){
-                check = di;
-                for (int k = 0; (k + di) < summl; k++){
-                    if (summ[k] > summ[k + di]){
-                        int tmp = summ[k];
-                        summ[k] = summ[k + di];
-                        summ[k + di] = tmp;
-                        //System.out.println("Change (ras): " +  izm++ + " " + Arrays.toString(summ));
-                    }
+        //Сортируем до значения фактора сравнения 2, потому как 2/1.247=1,6->2
+        while (shag_srav > 2) {
+            int tmp;
+            for (int i = 0; i + shag_srav < dlinna_itog_mas; i++) {
+                if (itogoviy_mas[i] > itogoviy_mas[i + shag_srav]) {
+                    tmp = itogoviy_mas[i];
+                    itogoviy_mas[i] = itogoviy_mas[i + shag_srav];
+                    itogoviy_mas[i + shag_srav] = tmp;
                 }
-                d = di / 1.247;
-                //System.out.println("d = " +  d + " " + Arrays.toString(summ));
-                di = (int) (d + 0.5);
-                //System.out.println("di = " +  di + " " + Arrays.toString(summ));
             }
-            else {
-                for(int t = 0; t < (summl - 1); t++){
-                    if (summ[t] > summ[t + 1]){
-                        int tmp = summ[t];
-                        summ[t] = summ[t + 1];
-                        summ[t + 1] = summ[t];
-                        //System.out.println("Change (puz): " +  izm++ + " " + Arrays.toString(summ));
-                    }
-                }
-                i = 1;
-            }
+            d = shag_srav / 1.247;
+            shag_srav = (int) (d + 0.5);
         }
 
-        /*for (int i = 0; i < (summl - 1); i++){
-           // Первый вариант, которому требуется 27 шагов цикла и 12 изменений в массиве [6, 8, 1, 3, 0, 0] для сортировки по возрастанию
-           //if (summ[i] > summ[i + 1]){
-           //    int tmp = summ[i];
-           //    summ[i] = summ[i + 1];
-           //    summ[i + 1] = tmp;
-           //    i = i - 2;
-           //    if (i == -2){
-           //        i++;
-           //    } 
-           //    izm++;
-           //    System.out.println("Change: " +  izm + " " + Arrays.toString(summ));
-           //}
-
-           // Второй вариант, которому требуется 5 шагов цикла и 12 изменений в массиве [6, 8, 1, 3, 0, 0] для сортировки по возрастанию
-           for (int k = i; k < (summl - 1); k++){
-               if (summ[i] > summ[k + 1]){
-                   int tmp = summ[i];
-                   summ[i] = summ[k + 1];
-                   summ[k + 1] = tmp;
-                   izm++;
-                   System.out.println("Change: " +  izm + " " + Arrays.toString(summ));
-               }
-           }
-           shag++;
-           //System.out.println("Step: " +  shag + " " + Arrays.toString(summ));
-        }*/
-        System.out.println(Arrays.toString(summ));
-        return summ;
+        //Досортировываем при значении фактора сравнения 2 и 1
+        while (shag_srav >= 1) {
+            int tmp;
+            for (int i = 0; i + shag_srav < dlinna_itog_mas; i++) {
+                if (itogoviy_mas[i] > itogoviy_mas[i + shag_srav]) {
+                    tmp = itogoviy_mas[i];
+                    itogoviy_mas[i] = itogoviy_mas[i + shag_srav];
+                    itogoviy_mas[i + shag_srav] = tmp;
+                }
+            }
+            shag_srav--;
+        }
+        return itogoviy_mas;
     }
 }
+
--- 2.4/stepic.java
+++ 2.4/stepic.java
@@ -1,17 +1,18 @@
 import java.util.Arrays;
 
 class stepic {
+
     public static void main(String[] args) {
         //System.out.println(s8.factorial(1));
         //System.out.println(s9.mergeArrays(new int[] {6, 8}, new int[] {1, 3, 0, 0}));
         //System.out.println(s9.mergeArrays(new int[] {}, new int[] {1, 5, 2, 0}));
         //System.out.println(s9.mergeArrays(new int[] {}, new int[] {}));
-        
+
         // Длинна тестового массива
         int dtm = 50;
-        int [] tmsj = new int [dtm];
-        int [] tmsi = new int [dtm];
-        
+        int[] tmsj = new int[dtm];
+        int[] tmsi = new int[dtm];
+
         // Заполняем массивы
         for (int i = 0; dtm > i; i++) {
             tmsj[i] = tmsi[i] = (int) (Math.random() * 10);
@@ -20,9 +21,13 @@ class stepic {
         System.out.println("Odinakovo?: " + Arrays.equals(tmsj, tmsi));
 
         Arrays.sort(tmsj);
+        System.out.println("Ne moya sortirovka");
         System.out.println(Arrays.toString(tmsj));
-        
-        System.out.println(s9.mergeArrays(tmsi, new int[] {}));
-        System.out.println(Arrays.equals(tmsj, tmsi));
+
+        System.out.println(Arrays.toString(s9.mergeArrays(tmsi, new int[]{})));
+        System.out.println("Rabotaet moya sortirovka:");
+        System.out.println(Arrays.equals(tmsj, s9.mergeArrays(tmsi, new int[]{})));
     }
+
 }
+
